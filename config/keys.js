49ce14fa44b24f7b3c1328ac73b834d4695f216a
zhangsheng <zhsheng12@gmail.com>
2018-03-08 01:13:31 -0800

    keys config added

---
+++ config/keys.js
@@ -0,0 +1,15 @@
+/*
+* @Author: zhangsheng
+* @Date:   2018-03-07 14:58:10
+* @Last Modified by:   zhangsheng
+* @Last Modified time: 2018-03-08 01:12:57
+*/
+
+module.exports = {
+  mongoURI: '',
+  pusherAppId: '',
+  pusherKey: '',
+  pusherSecret: '',
+  pusherCluster: '',
+  pusherEncrypted: true
+};
